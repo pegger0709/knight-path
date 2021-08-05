fb89be54364c8aec08cfdedc313a7597d7dfff2a
Philip Egger <philip.egger.1987@gmail.com>
2021-08-05 10:56:40 +0200

    Create functions for moving knight

--- knight.py
+++ knight.py
@@ -11,4 +11,26 @@ class Knight:
             self.square = square
         self.visited_squares = [self.square]
 
+    def getPossibleMoves(self):
+        i = self.square % 8
+        j = self.square // 8
+        legal_moves = []
+        for move in [(2,1),(1,2),(-1,2),(-2,1),(-2,-1),(-1,-2),(1,-2),(2,-1)]:
+            if 0 <= i+move[0] < 8 and 0 <= j+move[1] < 8:
+                destination = (j+move[1])*8 + (i+move[0])
+                if destination not in self.visited_squares:
+                    legal_moves.append(destination)
+        return legal_moves
+
+    def move(self, destination):
+        if destination not in self.getPossibleMoves():
+            print('Knight cannot reach square %d from square %d' % (destination, self.square))
+            pass
+        else:
+            self.square = destination
+            self.visited_squares.append(destination)
+
+    def startRandomWalk(self):
+        while self.getPossibleMoves():
+            self.move(random.choice(self.getPossibleMoves()))
 
