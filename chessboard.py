7fa5361c7a75da2841c1bf677f7cff3d7d53f413
Philip Egger <philip.egger.1987@gmail.com>
2021-08-08 10:58:23 +0200

    Add docstrings

--- chessboard.py
+++ chessboard.py
@@ -3,7 +3,38 @@ import matplotlib.pyplot as plt
 import string
 
 class Chessboard:
+    """
+    class Chessboard represents a rectangular chessboard for the knight to travel on
+
+    Attributes
+    ----------
+    nrank: int
+        the rank of the chessboard, i.e. the number of rows (default 8)
+    nfile: int
+        the file of the chessboard, i.e. the number of columns (default 8)
+
+    Methods
+    -------
+    plot()
+        prepares a matplotlib plot of the chessboard, in order to visualize the knight's tour
+    """
     def __init__(self, nrank=8, nfile=8):
+        """
+        Parameters
+        ----------
+        nrank: int
+            the rank of the chessboard, i.e. the number of rows (default 8)
+        nfile: int
+            the file of the chessboard, i.e. the number of columns (default 8)
+
+        Constraints
+        -----------
+        In 1991, A.J. Schwenk proved that any m * n chessboard with m <= n
+        admits a closed knight's tour unless one of the three following conditions hold:
+            1. m and n are both odd
+            2. m = 1, 2, or 4
+            3. m = 3 and n = 4, 6, or 8
+        """
         self.nrank = nrank
         self.nfile = nfile
         m = min([nrank, nfile])
@@ -16,9 +47,19 @@ class Chessboard:
             print('m=%d, n=%d: for m=3 and n=4, 6, or 8, a closed tour may not be possible' % (m,n))
         else:
             print('A closed tour is possible')
-    def plot(self):
+
+    def plot(self, show=False):
+        """
+        Parameters
+        ----------
+        show: bool
+            If True, then display the plot of the blank chessboard.
+            If False, then create the plot but do not display,
+            in order to draw arrows representing knight's tour (default)
+        """
         chessboard = np.array([[(i+j)%2 for i in range(self.nfile)] for j in range(self.nrank)])
         plt.imshow(chessboard, cmap='gray')
         plt.xticks(range(self.nfile),list(string.ascii_uppercase[:self.nfile]))
         plt.yticks(range(self.nrank),range(self.nrank,1-1,-1))
-
+        if show:
+            plt.show()
--- knight.py
+++ knight.py
@@ -4,7 +4,51 @@ import matplotlib.pyplot as plt
 from chessboard import Chessboard
 
 class Knight:
+    """
+    class Knight represents the knight that will perform a closed tour
+    on a given rectangular chessboard, visiting each square exactly once
+    in such a way that the end of the tour is a knight's move away from the start.
+
+    Attributes
+    ----------
+    chessboard: Chessboard object
+        The board on which the knight is to find a closed tour.
+    square: int
+        The square on which the knight currently stands
+    visited_squares: list of int
+        The list of all squares the knight has visited so far in its tour
+
+    Methods
+    -------
+    getPossibleMoves(visited=False)
+        Returns list of previously unvisited (or visited) squares accessible
+        in one knight's move from the current square
+    move(destination)
+        Moves the knight to the destination square
+    randomWalk()
+        Sends the knight on a random walk across unvisited squares
+    pivotPath(pivot)
+        Pivots the walk of the knight (todo: write better description)
+    unblock()
+        When the knight is stuck, continue pivoting until the knight becomes unstuck
+    traverseChessboard()
+        Perform a closed tour
+    plotTraversal():
+        Create a matplotlib plot depicting the knight's path thus far
+    """
     def __init__(self, chessboard=None, square=-1):
+        """
+        Parameters
+        ----------
+        chessboard: Chessboard object
+            The board on which the knight is to find a closed tour. (default 8*8)
+            The squares are labeled from 0 to rank * file - 1
+            starting at the upper left corner and traveling first along the top row.
+        square: int
+            The square on which the knight's tour begins. If -1, then a random initial square is chosen (default -1)
+        visited_squares: list of int
+            The list of all squares the knight has visited so far in its tour.
+        """
         self.chessboard = Chessboard() if chessboard is None else chessboard
         if square == -1:
             self.square = random.randint(0, self.chessboard.nrank * self.chessboard.nfile - 1)
@@ -15,7 +59,26 @@ class Knight:
         else:
             self.square = square
         self.visited_squares = [self.square]
+
     def getPossibleMoves(self, *, visited=False):
+        """
+        Finds all squares accessible to the knight from the current square by one knight's move
+
+        Parameters
+        ----------
+        visited: bool
+            If True, return the list of accessible squares which have already been visited
+            If False, return the list of accessible squares which have not already been visited
+
+        Returns
+        -------
+        list(int)
+            A list of integers corresponding to those visited (or unvisited)
+            squares reachable from the current square by a knight's move.
+            We want the unvisited squares if we're pursuing a tour, and we want
+            the visited squares if we're looking for a square to pivot on.
+
+        """
         i = self.square % self.chessboard.nfile
         j = self.square // self.chessboard.nrank
         legal_moves = []
@@ -27,17 +90,39 @@ class Knight:
                 elif visited and (destination in self.visited_squares):
                     legal_moves.append(destination)
         return legal_moves
+
     def move(self, destination):
+        """
+        Moves the knight to the given square, provided this is possible with one knight's move
+
+        Parameters
+        ----------
+        destination: int
+            The square to which we want to move the knight
+        """
         if destination not in self.getPossibleMoves():
             print('Knight cannot reach square %d from square %d' % (destination, self.square))
             pass
         else:
             self.square = destination
             self.visited_squares.append(destination)
+
     def randomWalk(self):
+        """
+        Sends the knight on a random walk as long as it can move on unvisited squares
+        """
         while self.getPossibleMoves(visited=False):
             self.move(random.choice(self.getPossibleMoves(visited=False)))
+
     def pivotPath(self, pivot):
+        """
+        Pivots the knight's tour on a given square (todo: write better description)
+
+        Parameters
+        ----------
+        pivot: int
+            The square to pivot on
+        """
         if pivot not in self.getPossibleMoves(visited=True):
             print('%d is not a valid pivot')
         else:
@@ -54,15 +139,27 @@ class Knight:
                 i -= 1
             self.visited_squares = new_path
             self.square = new_path[-1]
+
     def unblock(self):
+        """
+        When the knight is stuck, continue attempting to pivot until it can continue the tour
+        """
         pivot = random.choice(list(set(self.getPossibleMoves(visited=True)).difference({self.visited_squares[-2]})))
         self.pivotPath(pivot)
         self.randomWalk()
+
     def traverseChessboard(self):
+        """
+        Sends the knight on a complete knight's tour of the chessboard
+        """
         self.randomWalk()
         while len(self.visited_squares) < self.chessboard.nrank * self.chessboard.nfile:
             self.unblock()
+
     def plotTraversal(self):
+        """
+        Produces a matplotlib figure depicting the knight's tour on the chessboard
+        """
         self.chessboard.plot()
         for i in range(len(self.visited_squares) - 1):
             tail = self.visited_squares[i]
@@ -73,5 +170,3 @@ class Knight:
             y_head = head // self.chessboard.nrank
             plt.arrow(x_tail, y_tail, x_head-x_tail, y_head-y_tail, head_width=0.1, color='red')
         plt.show()
-
-
